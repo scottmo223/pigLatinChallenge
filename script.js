49a0a9efb9169c6ddc86c5f944203bd51ad69020
scottmo223 <scottmo223@yahoo.com>
2018-11-22 00:16:43 -0600

    Forgot to get rid of my cheater notes

--- script.js
+++ script.js
@@ -14,26 +14,6 @@ textBox.addEventListener("keyup", pressEnter => {
 	}
 });
 
-/* 
-// Get the input field
-var input = document.getElementById("myInput");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Cancel the default action, if needed
-  event.preventDefault();
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
-}); */
-
-
-
-
-
-
 //recieve input from text box
 function getWord() {
 	let textInput = document.getElementById("word").value;
